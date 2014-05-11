0a96c47a517b318324a74c6e782b34b8366bf9ad
Clément Mouline <clement.mouline@app-solutions.fr>
2014-05-11 16:14:19 +0200

    amélioration de personne

--- application/src/fr/kubrrickTutorial/simplelist/Personne.java
+++ application/src/fr/kubrrickTutorial/simplelist/Personne.java
@@ -6,14 +6,17 @@ public class Personne {
 	private String nom;
 	private String prenom;
 	private int age;
+	private String adresse;
+	private String telephone;
 	
 	
-	
-	public Personne(String nom, String prenom, int age) {
+	public Personne(String nom, String prenom, int age, String adresse, String telephone) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
 		this.age = age;
+		this.adresse = adresse;
+		this.telephone = telephone;
 	}
 
 
@@ -35,6 +38,12 @@ public class Personne {
 	}
 	
 	
+	public String getAdresse(){
+		return adresse;
+	}
 	
+	public String getTelephone(){
+		return telephone;
+	}
 
 }
