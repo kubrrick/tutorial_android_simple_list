3f743f71fd652d5566254fdaefc28045eeecfb00
Clément Mouline <clement.mouline@app-solutions.fr>
2014-05-11 16:20:39 +0200

    modification des personnes main

--- application/src/fr/kubrrickTutorial/simplelist/Main.java
+++ application/src/fr/kubrrickTutorial/simplelist/Main.java
@@ -32,16 +32,16 @@ public class Main extends ListActivity {
 	
 	private ArrayList<Personne> preparePersonneList(){
 		ArrayList<Personne> personneList = new ArrayList<Personne>();
-		personneList.add(new Personne("Mouline", "Clément", 22));
-		personneList.add(new Personne("Enderlin", "Morgan", 21));
-		personneList.add(new Personne("Barakowsky", "Baptiste", 22));
-		personneList.add(new Personne("Meyer", "Franck", 20));
-		personneList.add(new Personne("Zorn", "Nicolas", 22));
-		personneList.add(new Personne("Arnold", "Loïc", 22));
-		personneList.add(new Personne("Bruno", "Guillaume", 22));
-		personneList.add(new Personne("Ta", "Guillaume", 20));
-		personneList.add(new Personne("Jobs", "Steve", 56));
-		personneList.add(new Personne("Bauer", "Adrien", 22));
+		personneList.add(new Personne("Mouline", "Clément", 22, "14 cité Kermilliau 75000 PARIS", "0024867598"));
+		personneList.add(new Personne("Enderlin", "Morgan", 21, "14 cité Kermilliau 75000 PARIS", "0034586125"));
+		personneList.add(new Personne("Barakowsky", "Baptiste", 22, "14 cité Kermilliau 75000 PARIS", "0065847589"));
+		personneList.add(new Personne("Meyer", "Franck", 20, "14 cité Kermilliau 75000 PARIS", "0045896587"));
+		personneList.add(new Personne("Zorn", "Nicolas", 22, "14 cité Kermilliau 75000 PARIS", "0063254781"));
+		personneList.add(new Personne("Arnold", "Loïc", 22, "14 cité Kermilliau 75000 PARIS", "0045878962"));
+		personneList.add(new Personne("Bruno", "Guillaume", 22, "14 cité Kermilliau 75000 PARIS", "0045879523"));
+		personneList.add(new Personne("Ta", "Guillaume", 20, "14 cité Kermilliau 75000 PARIS", "0042647859"));
+		personneList.add(new Personne("Jobs", "Steve", 56, "14 cité Kermilliau 75000 PARIS", "0062145736"));
+		personneList.add(new Personne("Bauer", "Adrien", 22, "14 cité Kermilliau 75000 PARIS", "0042684589"));
 		return personneList;
 	}
 	
