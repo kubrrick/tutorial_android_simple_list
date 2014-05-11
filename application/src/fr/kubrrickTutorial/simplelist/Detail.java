4d181ce95f2050286b745b60b4f39efb79e85155
Clément Mouline <clement.mouline@app-solutions.fr>
2014-05-11 16:12:13 +0200

    création d'une nouvelle classe Detail

---
+++ application/src/fr/kubrrickTutorial/simplelist/Detail.java
@@ -0,0 +1,16 @@
+package fr.kubrrickTutorial.simplelist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Detail extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_detail);
+		
+	}
+	
+	
+}
